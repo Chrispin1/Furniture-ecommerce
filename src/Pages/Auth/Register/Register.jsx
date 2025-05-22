1609ca6077aa156e2a45f1b341235e42b5824e3f
Chrispin <kiokondolo1@gmail.com>
2025-05-22 23:17:09 +0300

    project complete

--- src/Pages/Auth/Register/Register.jsx
+++ src/Pages/Auth/Register/Register.jsx
@@ -1,5 +1,71 @@
+import { Link, useNavigate } from "react-router";
+
 const Register = () => {
-  return <div>Register</div>;
+  const navigate = useNavigate();
+
+  const submitHandle = () => {
+    navigate("/");
+  };
+
+  return (
+    <div>
+      <div className="lg:container mx-auto ">
+        <div className="w-full flex items-center justify-center pt-20 ">
+          <div className="min-w-[500px] py-10 px-10 border border-gray-400 rounded-lg bg-gray-50">
+            <div className="flex flex-col items-center justify-center w-full">
+              <h1 className="text-3xl font-semibold text-gray-800">Login</h1>
+              <div className="w-full pt-6">
+                <form onSubmit={submitHandle} className="w-full flex flex-col">
+                  <label htmlFor="name" className="px-2 text-md font-medium">
+                    Name
+                  </label>
+                  <input
+                    type="name"
+                    placeholder="Your Name..."
+                    className="border border-gray-400 w-full rounded-full px-3 py-3 focus:outline-[#029fae]"
+                  />
+                  <label
+                    htmlFor="email"
+                    className="px-2 text-md font-medium pt-4"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="Your Email..."
+                    className="border border-gray-400 w-full rounded-full px-3 py-3 focus:outline-[#029fae]"
+                  />
+                  <label
+                    htmlFor="password"
+                    className="px-2 text-md font-medium pt-4"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="Enter password..."
+                    className="border border-gray-400 w-full rounded-full px-3 py-3 focus:outline-[#029fae]"
+                  />
+                  <div className="flex items-center pt-4 ">
+                    <button className="bg-[#029fae] text-center text-white w-full py-2 rounded-full cursor-pointer">
+                      Login
+                    </button>
+                  </div>
+                </form>
+
+                <p className="text-md pt-4 text-gray-500 mb-6">
+                  Don't have an account?
+                  <Link to="/auth/register">
+                    <span className="text-[#029fae]">Register</span>
+                  </Link>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { Link } from "react-router";
 
 const Error = () => {
-  return <div>Hmmm...Page not Found</div>;
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center pt-[200px]">
+      <h1 className="text-5xl font-semibold">Hmmm...Page not Found</h1>
+      <button className="rounded-full bg-[#029fae] px-5 py-3">
+        <Link to="/">Go to Homepage</Link>
+      </button>
+    </div>
+  );
 };
 
 export default Error;
